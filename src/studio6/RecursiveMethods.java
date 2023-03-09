54e4863ca4b4613e16b3c02b65732076642a9e29
charliefallon <charliefallon@localhost>
2023-03-09 12:46:53 -0600

    finished recursive methods

--- src/studio6/RecursiveMethods.java
+++ src/studio6/RecursiveMethods.java
@@ -14,7 +14,14 @@ public class RecursiveMethods {
 	public static double geometricSum(int n) {
 		
 			// FIXME compute the geometric sum for the first n terms recursively
+		if (n == 0)
+		{
 			return 0;
+		}
+		else
+		{
+			return (double) 1 / (Math.pow(2, n)) + geometricSum(n-1);
+		}
 		
 	}
 
@@ -29,7 +36,14 @@ public class RecursiveMethods {
 	public static int gcd(int p, int q) {
 		
 			// FIXME compute the gcd of p and q using recursion
-			return 0;
+		if (p % q == 0)
+		{
+			return q;
+		}
+		else
+		{
+			return gcd(q, p % q);
+		}
 		
 	}
 
@@ -42,10 +56,34 @@ public class RecursiveMethods {
 	 * @return an array with the same data as the input but it reverse order
 	 */
 	public static int[] toReversed(int[] array) {
+		int[] copy = new int[array.length];
+		for (int i = 0; i < array.length; i++)
+		{
+			copy[i] = array[i];
+		}
 		
 			// FIXME create a helper method that can recursively reverse the given array
-			return new int[0];
-		
+//		if (array.length == 0 || array.length == 1)
+//		{
+//			return array;
+//		}
+//		else
+//		{
+			return reverse(copy, 0);
+//		}
+	}
+	public static int[] reverse(int[] array, int p) {
+		if (p == array.length/2)
+		{
+			return array;
+		}
+		else
+		{
+			int temp = array[array.length - 1 - p];
+			array[array.length - 1 - p] = array[p];
+			array[p] = temp;
+			return reverse(array, p++);
+		}
 	}
 
 	/**
